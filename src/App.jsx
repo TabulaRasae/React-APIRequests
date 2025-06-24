3d137316ef9c2465e74cb3cc643823ca96e75e8a
Bar Yaakov <baryaakov5050@gmail.com>
2025-06-24 13:56:49 -0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-// import axios from "axios"; // Maybe we'll need axios? 🤔
+import axios from "axios";
 import "./style.css";
+import SearchField from "./SearchField";
+import GifCard from "./GifCard";
 
-// const GIPHY_API_KEY = "YOUR_API_KEY";
+const GIPHY_API_KEY = "YAYxIpb6PDFuq6AhXfhUTitXSu1oY4rE";
 
 const App = () => {
+
   return (
     <div className="app">
       <h1 className="title">Let's Make Some API Requests!</h1>
+      <Generate/>
+      <SearchField/>
     </div>
+    
   );
 };
+
+const Generate = () => {
+
+
+  return (
+    <button className= "buttons" onClick = {""} > Generate Gif </button>
+
+  )
+};
+
+
 
 // The following lines initialize your React application and inject
 // it into the index.html
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
+
--- src/SearchField.jsx
+++ src/SearchField.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const SearchField = () => {
-  return <div></div>;
+  return (
+    <form onSubmit={handleSubmit} className="search-bar">
+      <input
+        type="text"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="search-input"
+      />
+      <button type="submit" className="search-button">
+        Search
+      </button>
+    </form>
+  );
 };
 
 export default SearchField;
