d4c17624470fe3c3a4a63b2a189f929222ca29c8
RamsesCode <RamsesSanchez25@gmail.com>
2025-06-24 15:02:00 -0400

    API working

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
-import { React, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import axios from "axios";
 import "./style.css";
 
+
 const GIPHY_API_KEY = `YAYxIpb6PDFuq6AhXfhUTitXSu1oY4rE`;
-const url = `http://api.giphy.com/v1/gifs/trending?api_key=${GIPHY_API_KEY}`;
+const url = `https://api.giphy.com/v1/gifs/trending?api_key=${GIPHY_API_KEY}`;
 
 const App = () => {
   const [gifs, setGifs] = useState([]);
+
   const fetchGifs = async () => {
-    const gifsResponse = await axios.get(url);
-    const gifsData = gifsResponse.data;
-    setGifs(gifsData);
+    try {
+      const gifsResponse = await axios.get(url);
+      const gifsData = gifsResponse.data.data;
+      setGifs(gifsData);
+    } catch (error) {
+      console.error("Error fetching gifs:", error);
+    }
   };
 
   useEffect(() => {
-    fetchGifs;
+    fetchGifs(); 
   }, []);
-  console.log("Gifs", gifs);
+
   return (
     <div className="app">
-      <h1 className="title">Let's Make Some API Requests!</h1>
+      <h1 className="title">Gif Generator!</h1>
       <button className="buttons" onClick={fetchGifs}>
         Generate Gif
       </button>
+
+      <ul className="gif-list">
+        {gifs.map((gif) => (
+          <li key={gif.id} className="gif-card">
+            <img src={gif.images.fixed_height.url} alt={gif.title} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-// The following lines initialize your React application and inject
-// it into the index.html
+
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
