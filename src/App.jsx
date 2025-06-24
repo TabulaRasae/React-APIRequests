f89c7ab13347bb75c7ca0d979512251a88046ab0
Bar Yaakov <baryaakov5050@gmail.com>
2025-06-24 14:38:48 -0400

    not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,34 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import axios from "axios";
 import "./style.css";
-import SearchField from "./SearchField";
-import GifCard from "./GifCard";
 
-const GIPHY_API_KEY = "YAYxIpb6PDFuq6AhXfhUTitXSu1oY4rE";
+const GIPHY_API_KEY = `YAYxIpb6PDFuq6AhXfhUTitXSu1oY4rE`;
+const url = `http://api.giphy.com/v1/gifs/trending?api_key=${GIPHY_API_KEY}`;
 
 const App = () => {
+  const [gifs, setGifs] = useState([]);
+  const fetchGifs = async () => {
+    const gifsResponse = await axios.get(url);
+    const gifsData = gifsResponse.data;
+    setGifs(gifsData);
+  };
 
+  useEffect(() => {
+    fetchGifs;
+  }, []);
+  console.log("Gifs", gifs);
   return (
     <div className="app">
       <h1 className="title">Let's Make Some API Requests!</h1>
-      <Generate/>
-      <SearchField/>
+      <button className="buttons" onClick={fetchGifs}>
+        Generate Gif
+      </button>
     </div>
-    
   );
 };
-
-const Generate = () => {
-
-
-  return (
-    <button className= "buttons" onClick = {""} > Generate Gif </button>
-
-  )
-};
-
-
 
 // The following lines initialize your React application and inject
 // it into the index.html
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
-
